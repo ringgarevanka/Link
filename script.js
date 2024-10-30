18e030fe8b250388a74ef29c8d6fdd11afdedc74
Ringga Revanka <99320495+ringgarevanka@users.noreply.github.com>
2024-10-30 22:42:22 +0700

    Update script.js
    Testing Optimized Code

--- script.js
+++ script.js
@@ -1,62 +1,98 @@
-fetch('data.json')
+// Main Function //
+// Caching data using Local Storage
+let cachedData = null;
+const dataKey = 'CachedData';
+
+function getCachedData() {
+  if (cachedData) {
+    return Promise.resolve(cachedData);
+  } else {
+    const cachedDataString = localStorage.getItem(dataKey);
+    if (cachedDataString) {
+      cachedData = JSON.parse(cachedDataString);
+      return Promise.resolve(cachedData);
+    } else {
+      return Promise.reject(new Error('Data not found in cache'));
+    }
+  }
+}
+
+function fetchAndCacheData() {
+  return fetch('data.json')
     .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
     })
     .then(data => {
-        // Filling data in HTML elements
-        document.title = data.title;
-
-        const authorMeta = document.querySelector('meta[name="author"]');
-        if (authorMeta) authorMeta.setAttribute('content', data.author);
-
-        const copyrightMeta = document.querySelector('meta[name="copyright"]');
-        if (copyrightMeta) copyrightMeta.setAttribute('content', data.copyright);
-
-        const descriptionMeta = document.querySelector('meta[name="description"]');
-        if (descriptionMeta) descriptionMeta.setAttribute('content', data.description);
-
-        const ogDescriptionMeta = document.querySelector('meta[property="og:description"]');
-        if (ogDescriptionMeta) ogDescriptionMeta.setAttribute('content', data.meta.ogDescription);
-
-        const ogImageMeta = document.querySelector('meta[property="og:image"]');
-        if (ogImageMeta) ogImageMeta.setAttribute('content', data.meta.ogImage);
-
-        const ogTitleMeta = document.querySelector('meta[property="og:title"]');
-        if (ogTitleMeta) ogTitleMeta.setAttribute('content', data.meta.ogTitle);
-
-        const appleTouchIconLink = document.querySelector('link[rel="apple-touch-icon"]');
-        if (appleTouchIconLink) appleTouchIconLink.setAttribute('href', data.meta.appleTouchIcon);
-
-        const faviconLink = document.querySelector('link[rel="icon"]');
-        if (faviconLink) faviconLink.setAttribute('href', data.meta.favicon);
-
-        const profilePictureImg = document.querySelector('#profilePicture img');
-        if (profilePictureImg) profilePictureImg.setAttribute('src', data.profilePicture);
-
-        const userNameElem = document.getElementById('userName');
-        if (userNameElem) userNameElem.innerText = data.userName;
-
-        const hashtagElem = document.getElementById('hashtag');
-        if (hashtagElem) hashtagElem.innerText = data.hashtag;
-
-        // Fill in the data in the links section
-        const linksDiv = document.getElementById('links');
-        if (linksDiv) {
-            data.links.forEach(link => {
-                const a = document.createElement('a');
-                a.className = 'link';
-                a.href = link.href;
-                a.target = '_blank';
-                a.innerHTML = `<i class="${link.icon}">&nbsp;</i>${link.text}`;
-                linksDiv.appendChild(a);
-            });
-        }
+      // Caching data in Local Storage
+      localStorage.setItem(dataKey, JSON.stringify(data));
+      cachedData = data;
+      return data;
     })
     .catch(error => console.error('Error fetching data:', error));
+}
 
+// Function to update HTML elements
+function updateHTMLElement(selector, value) {
+  const element = document.querySelector(selector);
+  if (element) {
+    element.setAttribute('content', value);
+  }
+}
+
+getCachedData()
+  .then(data => {
+    // Filling data in HTML elements
+    document.title = data.title;
+    updateHTMLElement('meta[name="author"]', data.author);
+    updateHTMLElement('meta[name="copyright"]', data.copyright);
+    updateHTMLElement('meta[name="description"]', data.description);
+    updateHTMLElement('meta[property="og:description"]', data.meta.ogDescription);
+    updateHTMLElement('meta[property="og:image"]', data.meta.ogImage);
+    updateHTMLElement('meta[property="og:title"]', data.meta.ogTitle);
+    updateHTMLElement('link[rel="apple-touch-icon"]', data.meta.appleTouchIcon);
+    updateHTMLElement('link[rel="icon"]', data.meta.favicon);
+    const profilePictureImg = document.querySelector('#profilePicture img');
+    if (profilePictureImg) profilePictureImg.setAttribute('src', data.profilePicture);
+    const userNameElem = document.getElementById('userName');
+    if (userNameElem) userNameElem.innerText = data.userName;
+    const hashtagElem = document.getElementById('hashtag');
+    if (hashtagElem) hashtagElem.innerText = data.hashtag;
+
+    // Fill in the data in the links section
+    const linksDiv = document.getElementById('links');
+    if (linksDiv) {
+      data.links.forEach(link => {
+        const a = document.createElement('a');
+        a.className = 'link';
+        a.href = link.href;
+        a.target = '_blank';
+        a.innerHTML = `<i class="${link.icon}">&nbsp;</i>${link.text}`;
+        linksDiv.appendChild(a);
+      });
+    }
+  })
+  .catch(error => {
+    console.error('Error retrieving data:', error);
+    // Show clearer error messages to users
+    const errorElement = document.getElementById('errorMessage');
+    if (errorElement) {
+      errorElement.textContent = 'An error occurred while loading data. Please try again later.';
+    }
+  });
+
+// If the data is not in the cache, fetch it from the server and save it to the cache.
+if (!cachedData) {
+  fetchAndCacheData()
+    .catch(error => console.error('Error fetching and caching data:', error));
+}
+// End Of Main Function //
+
+// --- //
+
+// Additional Functions //
 // Disable right-click context menu
 document.addEventListener('contextmenu', event => event.preventDefault());
 
@@ -164,3 +200,4 @@ const handlePaste = event => event.preventDefault();
 document.addEventListener('keydown', handleKeyboardShortcuts);
 document.addEventListener('paste', handlePaste);
 */
+// End of Additional Functions //
